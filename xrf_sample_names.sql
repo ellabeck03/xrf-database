c411ddffd5e910407026685e4fd4cda96b5e6ad8
Ella Beck <127053515+ellabeck03@users.noreply.github.com>
2025-05-08 15:19:10 +0100

    sample data to be read into xrf_results.db

---
+++ xrf_sample_names.sql
@@ -0,0 +1,50 @@
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Sulfur Box', '28-02-2025', NULL, NULL, NULL, NULL, 'Sample scanned from within box');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Pellets (Catalogue No. J.M.703)', '28-02-2025', NULL, NULL, NULL, NULL,  'Sample scanned from within container');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Container (Labelled Cr pellets)', '28-02-2025', NULL, NULL, NULL, NULL, 'Sample scanned from within container');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Ag Strip', '28-02-2025', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Scintillator 1', '28-02-2025', NULL, NULL, NULL, NULL, 'Sample scanned from transparent side');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Scintillator 2', '28-02-2025', NULL, NULL, NULL, NULL, 'Sample scanned from transparent side');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Scintillator 3', '28-02-2025', NULL, NULL, NULL, NULL, 'Sample scanned from transparent side');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Metal Cubes (Bi Box)', '28-02-2025', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Cd Foil', '20-02-2025', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Bi Puck', '20-02-2025', NULL, NULL, NULL, NULL, 'Sample scanned from within plastic bag');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Al Rods', '20-02-2025', NULL, NULL, NULL, NULL, 'Composition of all 4 rods is almost identical');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Non-Active Au Foils', '21-02-2025', NULL, NULL, NULL, NULL, 'Foils scanned stacked together from within plastic bags, with shiny side facing XRF gun');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Sample Pit A (Au Box)', '21-02-2025', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Sample Pit B (Au Box)', '21-02-2025', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Active Au Foils', '21-02-2025', NULL, NULL, NULL, NULL, 'Foils scanned outside box in low density plastic container');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Metal Components', '21-02-2025', NULL, NULL, NULL, NULL, 'Components stacked before being placed in the XRF');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Shiny Foils (Indium Box)', '21-02-2025', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Dull Foils (Indium Box)', '21-02-2025', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Chips (Indium Box)', '21-02-2025', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Unknown Sculpture', '21-02-2025', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Unknown Liquid', '21-02-2025', NULL, NULL, NULL, NULL, 'Sample scanned from bottom of container (therefore reading is likely from paint), scanned in regulated goods mode');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Crystals (Bi Box)', '28-02-2025', NULL, NULL, NULL, NULL, 'Both crystals almost identical in composition');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Rusty Metal Sheet (shiny side) (Bi Box)', '20-02-2025', NULL, NULL, NULL, NULL, 'Sample scanned from non-rusty side');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Rusty Metal Sheet (rusty side) (Bi Box)', '21-02-2025', NULL, NULL, NULL, NULL, 'Sample scanned from rusty side');
+INSERT INTO Samples (SampleName, CollectionDate, Mass_g, Length_mm, Width_mm, Height_mm, Notes)
+VALUES ('Mirror (Bi Box)', '21-02-2025', NULL, NULL, NULL, NULL, 'Sample scanned from reflective side');
