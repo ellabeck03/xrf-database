bdc2ce7c70a3b1657cb1b700e1235a4a0f03a13b
Ella Beck <127053515+ellabeck03@users.noreply.github.com>
2025-05-08 15:19:50 +0100

    results to be read into xrf_results.db

---
+++ xrf_sample_results.sql
@@ -0,0 +1,324 @@
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (1, 26, 50, 1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (1, 16, 22.1, 0.3);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (1, 46, 10.1, 0.7);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (1, 30, 3.9, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (1, 14, 3.1, 0.5);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (1, 42, 0.77, 0.22);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (1, 28, 0.72, 0.13);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (1, 73, 0.68, 0.15);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (1, 41, 0.61, 0.20);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (1, 74, 0.56, 0.13);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (1, 72, 0.47, 0.14);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (1, 77, 0.28, 0.07);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (2, 30, 99.9, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (2, 14, 0.08, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (2, 15, 0.024, 0.006);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 14, 60.7, 0.7);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 16, 16.8, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 46, 5.0, 0.3);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 23, 3.5, 0.7);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 33, 2.29, 0.07);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 30, 2.3, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 26, 2.0, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 13, 2.0, 0.7);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 47, 1.8, 0.3);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 79, 1.12, 0.09);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 40, 0.94, 0.07);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 41, 0.27, 0.07);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 74, 0.19, 0.06);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 75, 0.17, 0.05);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 77, 0.17, 0.04);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (3, 29, 0.11, 0.04);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (4, 47, 98.4, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (4, 13, 1.31, 0.04);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (4, 48, 0.15, 0.03);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (4, 29, 0.115, 0.008);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (5, 46, 55, 1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (5, 47, 15, 1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (5, 48, 7, 2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (5, 42, 5.3, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (5, 41, 4.0, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (5, 79, 3.5, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (5, 14, 3.4, 0.3);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (5, 40, 3.0, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (5, 39, 2.0, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (5, 26, 1.5, 0.3);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (5, 30, 0.69, 0.09);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (5, 16, 0.28, 0.08);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 46, 52, 1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 47, 12, 1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 48, 8, 1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 42, 5.8, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 41, 4.6, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 79, 3.6, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 40, 3.5, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 39, 2.3, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 26, 2.0, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 14, 1.5, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 72, 0.9, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 27, 0.8, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 73, 0.8, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 74, 0.67, 0.09);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 30, 0.66, 0.06);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 28, 0.48, 0.07);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (6, 29, 0.17, 0.03);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 46, 53, 1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 47, 12, 1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 48, 6, 1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 42, 4.9, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 41, 4.2, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 79, 3.7, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 40, 2.7, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 24, 2.2, 0.7);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 26, 2.1, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 39, 1.8, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 25, 1.3, 0.4);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 72, 1.2, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 73, 1.0, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 14, 0.9, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 74, 0.9, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 27, 0.8, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 30, 0.55, 0.06);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 28, 0.53, 0.07);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 75, 0.28, 0.06);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 77, 0.10, 0.03);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 78, 0.08, 0.03);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (7, 29, 0.06, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (8, 83, 99.5, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (8, 82, 0.29, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (8, 13, 0.26, 0.08);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (9, 48, 100.0, 0.5);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (10, 83, 99.5, 0.3);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (10, 82, 0.49, 0.03);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (11, 13, 99.6, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (11, 16, 0.221, 0.004);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (11, 14, 0.13, 0.01);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (12, 79, 99.9, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (12, 78, 0.12, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (13, 48, 99.9, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (13, 16, 0.054, 0.003);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (13, 15, 0.035, 0.006);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (14, 48, 99.8, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (14, 16, 0.1, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (14, 14, 0.099, 0.007);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (14, 15, 0.032, 0.004);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (15, 79, 98.1, 0.4);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (15, 80, 0.89, 0.05);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (15, 78, 0.29, 0.04);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (15, 34, 0.26, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (15, 14, 0.22, 0.04);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (16, 48, 99.8, 0.3);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (16, 26, 0.08, 0.03);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (16, 14, 0.07, 0.03);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (16, 16, 0.012, 0.005);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (17, 49, 98.9, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (17, 13, 0.39, 0.06);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (17, 14, 0.29, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (17, 47, 0.19, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (17, 48, 0.14, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (17, 15, 0.077, 0.005);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (18, 48, 99.9, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (18, 14, 0.11, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (19, 49, 96.5, 0.4);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (19, 50, 2.3, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (19, 46, 0.41, 0.04);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (19, 48, 0.34, 0.06);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (19, 79, 0.30, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (19, 14, 0.14, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (20, 82, 96.9, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (20, 16, 3.06, 0.01);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (20, 83, 0.08, 0.01);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (21, 22, 17.18, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (21, 40, 0.0500, 0.0004);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (21, 79, 0.033, 0.001);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (21, 23, 0.005, 0.0009);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (21, 26, 0.020, 0.005);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (21, 20, 0.015, 0.002);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (21, 82, 0.0060, 0.0005);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (21, 30, 0.0025, 0.0006);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (21, 83, 0.0020, 0.0004);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (21, 33, 0.0005, 0.0002);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (22, 83, 99.6, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (22, 82, 0.22, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (22, 26, 0.16, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (23, 83, 95.3, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (23, 50, 4.5, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (23, 82, 0.15, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (24, 29, 89.0, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (24, 83, 8.85, 0.06);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (24, 50, 1.56, 0.03);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (24, 14, 0.23, 0.02);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (24, 30, 0.153, 0.006);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (24, 82, 0.14, 0.01);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (24, 74, 0.046, 0.007);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (25, 14, 81.6, 0.2);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (25, 13, 15.3, 0.1);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (25, 12, 1.9, 0.3);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (25, 16, 0.57, 0.01);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (25, 33, 0.297, 0.005);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (25, 26, 0.18, 0.01);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (25, 75, 0.015, 0.002);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (25, 74, 0.006, 0.003);
+INSERT INTO Measurements (SampleID, ElementID, Concentration, Uncertainty)
+VALUES (25, 28, 0.005, 0.002);
